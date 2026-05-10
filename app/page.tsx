09f45ea631d09baae642ef7f5f1140be24912196
yeticlass <sangbu075@gmail.com>
2026-05-10 21:29:22 +0545

    test-3

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
           <div className="flex flex-col">
 
           <h1 className="text-2xl font-bold tracking-wide pl-5">
-            Miteri False Ceiling
+            Miteri Home Decor
           </h1>
           <p className="text-sm text-gray-500 pl-5">The name you trust</p>
         </div>
