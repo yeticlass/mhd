aaf73f2ba9c3ae7b02cbb8a6cc121522fe2660e9
Sangbu Sherpa <sangbu075@gmail.com>
2026-05-20 18:05:06 +0545

    updated

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -5,29 +5,7 @@ export default function ContactSection() {
   return (
     
     <section className="bg-gray-100 px-6">
-        <div>
-            <nav className="w-full flex items-center justify-between px-8 py-5 shadow-sm bg-white sticky top-0 z-50">
-        <div className="flex">
-            <Link href="/">
-          <img src='/images/logo.jpg' alt="Miteri Logo"  className=" rounded-lg" width={100} height={100} />
-          </Link>
-          <div className="flex flex-col">
-
-          <h1 className="text-2xl font-bold tracking-wide pl-5">
-            Miteri Home Decor
-          </h1>
-          <p className="text-sm text-gray-500 pl-5">The name you trust</p>
-        </div>
-        </div>
-
-        <ul className="hidden md:flex gap-8 font-medium">
-          <li className="hover:text-gray-500 cursor-pointer">Home</li>
-          <li className="hover:text-gray-500 cursor-pointer">Services</li>
-          <li className="hover:text-gray-500 cursor-pointer">Projects</li>
-          <li className="hover:text-gray-500 cursor-pointer">Contact</li>
-        </ul>
-      </nav>
-        </div>
+        
       <div className="max-w-6xl mx-auto grid md:grid-cols-2 gap-10 mt-10">
 
         {/* LEFT GRID */}
